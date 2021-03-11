dd6e3f8fd2a5b2f6acd32ea73b3cff33bcfcc9c2
Ben <Fuzzinator12@gmail.com>
2021-03-10 19:25:30 -0500

    added project
    what exists so far at least

---
+++ Assets/Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = quaternion.identity;
+    }
+}
