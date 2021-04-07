6ca7ef47a0f812a07b4f76a218f01ee1ac11e183
Ben <Fuzzinator12@gmail.com>
2021-04-07 13:19:12 -0400

    scene manager

---
+++ Assets/Scripts/Core/FalloutSceneManager.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Core
+{
+    public class FalloutSceneManager : MonoBehaviour
+    {
+        public static FalloutSceneManager Instance { get; private set; }
+
+        /// <summary>
+        /// Invoked when a scene is loaded. The scene is always the new scene, the first SceneType is the previous scene
+        /// and the second scene type is the new scene
+        /// </summary>
+        public static Action<Scene, SceneType, SceneType> sceneLoaded;
+
+        private static SceneType _currentSceneType;
+        private static Location _currentLocation;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(this);
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+
+        public static void SetLocation(Location location, Map map)
+        {
+            
+        }
+
+        public static void LoadNewScene(int buildIndex, SceneType sceneType, bool additive = false)
+        {
+            var currentScene = SceneManager.GetActiveScene();
+            var currentType = _currentSceneType;
+            _currentSceneType = sceneType;
+            var param = additive ? LoadSceneMode.Additive : LoadSceneMode.Single;
+            var loadScene = SceneManager.LoadSceneAsync(buildIndex, param);
+            loadScene.completed += (op) =>
+            {
+                sceneLoaded?.Invoke(SceneManager.GetSceneAt(buildIndex), currentType, sceneType);
+            };
+        }
+
+        public enum SceneType
+        {
+            None = 0,
+            Menu = 1,
+            Game = 2,
+            Map = 3
+        }
+    }
+}
--- Assets/Scripts/Core/WorldClock.cs
+++ Assets/Scripts/Core/WorldClock.cs
@@ -89,7 +89,7 @@ public class WorldClock : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(RunClock());
+        //StartCoroutine(RunClock());
     }
 
     private IEnumerator RunClock()
